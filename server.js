d96279c710d4e20615532f773eec823741ac2e70
Anhar Athoi <anharzihan@gmail.com>
2018-12-18 18:19:19 +1100

    Added cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
+const app = express();
+app.use(cors());
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-const app = express();
 const router = express.Router();
 mongoose.connect('mongodb://localhost:27017/artists')
 .then(() => console.log('MongoDB Connected'))
@@ -12,7 +14,7 @@ const port = 5000;
 app.get("/", (req, res) => {
   Artist.find({})
   .then(docs => {
-    res.send(docs)
+    res.json(docs[0])
   })
 });
 
